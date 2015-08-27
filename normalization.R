bf839ee4c7e938d7b747ef72d78c013b1c22c720
Don Armstrong <don@donarmstrong.com>
2015-08-27 09:02:03 -0700

    tweak BMIQ to have correct indenting

--- normalization.R
+++ normalization.R
@@ -354,222 +354,227 @@ BMIQcalibration <- function(datM,goldstandard.beta,nL=3,doH=TRUE,nfit=20000,th1.
 
 
 
-BMIQ = function(beta.v,design.v,nL=3,doH=TRUE,nfit=50000,th1.v=c(0.2,0.75),th2.v=NULL,niter=5,tol=0.001,plots=TRUE,sampleID=1,calibrateUnitInterval=TRUE){
+BMIQ <- function(beta.v,design.v,nL=3,doH=TRUE,nfit=50000,th1.v=c(0.2,0.75),th2.v=NULL,niter=5,tol=0.001,plots=TRUE,sampleID=1,calibrateUnitInterval=TRUE){
 
-if (calibrateUnitInterval) {
-rangeBySample=range(beta.v,na.rm=TRUE)
-minBySample=rangeBySample[1]
-maxBySample=rangeBySample[2]
-if ( (minBySample<0 | maxBySample>1) & !is.na(minBySample) & !is.na(maxBySample) ) {
-y1=c(0.001,.999) 
-x1=c(minBySample,maxBySample)
-lm1=lm( y1 ~ x1 )
-intercept1=coef(lm1)[[1]]
-slope1=coef(lm1)[[2]]
-beta.v=intercept1+slope1*beta.v
-} # end of if
-} # end of if (calibrateUnitInterval
-
-
-type1.idx = which(design.v==1);
-type2.idx = which(design.v==2);
-
-beta1.v = beta.v[type1.idx];
-beta2.v = beta.v[type2.idx];
+    if (calibrateUnitInterval) {
+        rangeBySample=range(beta.v,na.rm=TRUE)
+        minBySample=rangeBySample[1]
+        maxBySample=rangeBySample[2]
+        if ( (minBySample<0 | maxBySample>1) & !is.na(minBySample) & !is.na(maxBySample) ) {
+            y1=c(0.001,.999) 
+            x1=c(minBySample,maxBySample)
+            lm1=lm( y1 ~ x1 )
+            intercept1=coef(lm1)[[1]]
+            slope1=coef(lm1)[[2]]
+            beta.v=intercept1+slope1*beta.v
+        } # end of if
+    } # end of if (calibrateUnitInterval
 
 
-### estimate initial weight matrix from type1 distribution
-w0.m = matrix(0,nrow=length(beta1.v),ncol=nL);
-w0.m[which(beta1.v <= th1.v[1]),1] = 1;
-w0.m[intersect(which(beta1.v > th1.v[1]),which(beta1.v <= th1.v[2])),2] = 1;
-w0.m[which(beta1.v > th1.v[2]),3] = 1;
+    type1.idx = which(design.v==1);
+    type2.idx = which(design.v==2);
 
-### fit type1
-print("Fitting EM beta mixture to goldstandard probes");
-set.seed(1)
-rand.idx = sample(1:length(beta1.v),min(c(nfit, length(beta1.v))  )   ,replace=FALSE)
-em1.o = blc2(Y=matrix(beta1.v[rand.idx],ncol=1),w=w0.m[rand.idx,],maxiter=niter,tol=tol);
-subsetclass1.v = apply(em1.o$w,1,which.max);
-subsetth1.v = c(mean(max(beta1.v[rand.idx[subsetclass1.v==1]]),min(beta1.v[rand.idx[subsetclass1.v==2]])),mean(max(beta1.v[rand.idx[subsetclass1.v==2]]),min(beta1.v[rand.idx[subsetclass1.v==3]],na.rm=TRUE)));
-class1.v = rep(2,length(beta1.v));
-class1.v[which(beta1.v < subsetth1.v[1])] = 1;
-class1.v[which(beta1.v > subsetth1.v[2])] = 3;
-nth1.v = subsetth1.v;
-print("Done");
-
-### generate plot from estimated mixture
-if(plots){
-print("Check");
-tmpL.v = as.vector(rmultinom(1:nL,length(beta1.v),prob=em1.o$eta));
-tmpB.v = vector();
-for(l in 1:nL){
-  tmpB.v = c(tmpB.v,rbeta(tmpL.v[l],em1.o$a[l,1],em1.o$b[l,1]));
-}
-
-pdf(paste("Type1fit-",sampleID,".pdf",sep=""),width=6,height=4);
-plot(density(beta1.v));
-d.o = density(tmpB.v);
-points(d.o$x,d.o$y,col="green",type="l")
-legend(x=0.5,y=3,legend=c("obs","fit"),fill=c("black","green"),bty="n");
-dev.off();
-}
+    beta1.v = beta.v[type1.idx];
+    beta2.v = beta.v[type2.idx];
 
 
+    ## estimate initial weight matrix from type1 distribution
+    w0.m = matrix(0,nrow=length(beta1.v),ncol=nL);
+    w0.m[which(beta1.v <= th1.v[1]),1] = 1;
+    w0.m[intersect(which(beta1.v > th1.v[1]),which(beta1.v <= th1.v[2])),2] = 1;
+    w0.m[which(beta1.v > th1.v[2]),3] = 1;
+    
+    ## fit type1
+    print("Fitting EM beta mixture to goldstandard probes");
+    set.seed(1)
+    rand.idx = sample(1:length(beta1.v),min(c(nfit, length(beta1.v))  )   ,replace=FALSE)
+    em1.o = blc2(Y=matrix(beta1.v[rand.idx],ncol=1),w=w0.m[rand.idx,],maxiter=niter,tol=tol);
+    subsetclass1.v = apply(em1.o$w,1,which.max);
+    subsetth1.v =
+        c(mean(max(beta1.v[rand.idx[subsetclass1.v==1]]),
+               min(beta1.v[rand.idx[subsetclass1.v==2]])),
+          mean(max(beta1.v[rand.idx[subsetclass1.v==2]]),
+               min(beta1.v[rand.idx[subsetclass1.v==3]],na.rm=TRUE)));
+    class1.v = rep(2,length(beta1.v));
+    class1.v[which(beta1.v < subsetth1.v[1])] = 1;
+    class1.v[which(beta1.v > subsetth1.v[2])] = 3;
+    nth1.v = subsetth1.v;
+    print("Done");
 
-### Estimate Modes 
-if (  sum(class1.v==1)==1 ){ mod1U= beta1.v[class1.v==1]}
-if (  sum(class1.v==3)==1 ){ mod1M= beta1.v[class1.v==3]}
-if (  sum(class1.v==1) >1){ 
-d1U.o = density(beta1.v[class1.v==1])
-mod1U = d1U.o$x[which.max(d1U.o$y)]
-}
-if (  sum(class1.v==3)>1 ){ 
-d1M.o = density(beta1.v[class1.v==3])
-mod1M = d1M.o$x[which.max(d1M.o$y)]
-}
+    ## generate plot from estimated mixture
+    if(plots){
+        print("Check");
+        tmpL.v = as.vector(rmultinom(1:nL,length(beta1.v),prob=em1.o$eta));
+        tmpB.v = vector();
+        for(l in 1:nL){
+            tmpB.v = c(tmpB.v,rbeta(tmpL.v[l],em1.o$a[l,1],em1.o$b[l,1]));
+        }
 
-
-d2U.o = density(beta2.v[which(beta2.v<0.4)]);
-d2M.o = density(beta2.v[which(beta2.v>0.6)]);
-mod2U = d2U.o$x[which.max(d2U.o$y)]
-mod2M = d2M.o$x[which.max(d2M.o$y)]
-
-
-### now deal with type2 fit
-th2.v = vector();
-th2.v[1] = nth1.v[1] + (mod2U-mod1U);
-th2.v[2] = nth1.v[2] + (mod2M-mod1M);
-
-### estimate initial weight matrix 
-w0.m = matrix(0,nrow=length(beta2.v),ncol=nL);
-w0.m[which(beta2.v <= th2.v[1]),1] = 1;
-w0.m[intersect(which(beta2.v > th2.v[1]),which(beta2.v <= th2.v[2])),2] = 1;
-w0.m[which(beta2.v > th2.v[2]),3] = 1;
-
-print("Fitting EM beta mixture to input probes");
-set.seed(1)
-rand.idx = sample(1:length(beta2.v),min(c(nfit, length(beta2.v)),na.rm=TRUE)   ,replace=FALSE)
-em2.o = blc2(Y=matrix(beta2.v[rand.idx],ncol=1),w=w0.m[rand.idx,],maxiter=niter,tol=tol);
-print("Done");
-
-### for type II probes assign to state (unmethylated, hemi or full methylation)
-subsetclass2.v = apply(em2.o$w,1,which.max);
+        pdf(paste("Type1fit-",sampleID,".pdf",sep=""),width=6,height=4);
+        plot(density(beta1.v));
+        d.o = density(tmpB.v);
+        points(d.o$x,d.o$y,col="green",type="l")
+        legend(x=0.5,y=3,legend=c("obs","fit"),fill=c("black","green"),bty="n");
+        dev.off();
+    }
 
 
 
-if (sum(subsetclass2.v==2)>0 ){
-subsetth2.v = c(mean(max(beta2.v[rand.idx[subsetclass2.v==1]]),min(beta2.v[rand.idx[subsetclass2.v==2]])),
-mean(max(beta2.v[rand.idx[subsetclass2.v==2]]),min(beta2.v[rand.idx[subsetclass2.v==3]])));
-}
-if (sum(subsetclass2.v==2)==0 ){
-subsetth2.v = c(1/2*max(beta2.v[rand.idx[subsetclass2.v==1]])+ 1/2*mean(beta2.v[rand.idx[subsetclass2.v==3]]), 1/3*max(beta2.v[rand.idx[subsetclass2.v==1]])+ 2/3*mean(beta2.v[rand.idx[subsetclass2.v==3]]));
-}
+    ## Estimate Modes 
+    if (  sum(class1.v==1)==1 ){ mod1U= beta1.v[class1.v==1]}
+    if (  sum(class1.v==3)==1 ){ mod1M= beta1.v[class1.v==3]}
+    if (  sum(class1.v==1) >1){ 
+        d1U.o = density(beta1.v[class1.v==1])
+        mod1U = d1U.o$x[which.max(d1U.o$y)]
+    }
+    if (  sum(class1.v==3)>1 ){ 
+        d1M.o = density(beta1.v[class1.v==3])
+        mod1M = d1M.o$x[which.max(d1M.o$y)]
+    }
+    
+
+    d2U.o = density(beta2.v[which(beta2.v<0.4)]);
+    d2M.o = density(beta2.v[which(beta2.v>0.6)]);
+    mod2U = d2U.o$x[which.max(d2U.o$y)]
+    mod2M = d2M.o$x[which.max(d2M.o$y)]
+    
+
+    ## now deal with type2 fit
+    th2.v = vector();
+    th2.v[1] = nth1.v[1] + (mod2U-mod1U);
+    th2.v[2] = nth1.v[2] + (mod2M-mod1M);
+    
+    ## estimate initial weight matrix 
+    w0.m = matrix(0,nrow=length(beta2.v),ncol=nL);
+    w0.m[which(beta2.v <= th2.v[1]),1] = 1;
+    w0.m[intersect(which(beta2.v > th2.v[1]),which(beta2.v <= th2.v[2])),2] = 1;
+    w0.m[which(beta2.v > th2.v[2]),3] = 1;
+
+    print("Fitting EM beta mixture to input probes");
+    set.seed(1)
+    rand.idx = sample(1:length(beta2.v),min(c(nfit, length(beta2.v)),na.rm=TRUE)   ,replace=FALSE)
+    em2.o = blc2(Y=matrix(beta2.v[rand.idx],ncol=1),w=w0.m[rand.idx,],maxiter=niter,tol=tol);
+    print("Done");
+
+    ## for type II probes assign to state (unmethylated, hemi or full methylation)
+    subsetclass2.v = apply(em2.o$w,1,which.max);
+    
+
+    
+    if (sum(subsetclass2.v==2)>0 ){
+        subsetth2.v = c(mean(max(beta2.v[rand.idx[subsetclass2.v==1]]),
+            min(beta2.v[rand.idx[subsetclass2.v==2]])),
+            mean(max(beta2.v[rand.idx[subsetclass2.v==2]]),
+                 min(beta2.v[rand.idx[subsetclass2.v==3]])));
+    }
+    if (sum(subsetclass2.v==2)==0 ){
+        subsetth2.v = c(1/2*max(beta2.v[rand.idx[subsetclass2.v==1]])+ 1/2*mean(beta2.v[rand.idx[subsetclass2.v==3]]), 1/3*max(beta2.v[rand.idx[subsetclass2.v==1]])+ 2/3*mean(beta2.v[rand.idx[subsetclass2.v==3]]));
+    }
 
 
-class2.v = rep(2,length(beta2.v));
-class2.v[which(beta2.v <= subsetth2.v[1])] = 1;
-class2.v[which(beta2.v >= subsetth2.v[2])] = 3;
+    class2.v = rep(2,length(beta2.v));
+    class2.v[which(beta2.v <= subsetth2.v[1])] = 1;
+    class2.v[which(beta2.v >= subsetth2.v[2])] = 3;
 
 
 ### generate plot
-if(plots){
-tmpL.v = as.vector(rmultinom(1:nL,length(beta2.v),prob=em2.o$eta));
-tmpB.v = vector();
-for(lt in 1:nL){
-  tmpB.v = c(tmpB.v,rbeta(tmpL.v[lt],em2.o$a[lt,1],em2.o$b[lt,1]));
-}
-pdf(paste("Type2fit-",sampleID,".pdf",sep=""),width=6,height=4);
-plot(density(beta2.v));
-d.o = density(tmpB.v);
-points(d.o$x,d.o$y,col="green",type="l")
-legend(x=0.5,y=3,legend=c("obs","fit"),fill=c("black","green"),bty="n");
-dev.off();
-}
+    if(plots){
+        tmpL.v = as.vector(rmultinom(1:nL,length(beta2.v),prob=em2.o$eta));
+        tmpB.v = vector();
+        for(lt in 1:nL){
+            tmpB.v = c(tmpB.v,rbeta(tmpL.v[lt],em2.o$a[lt,1],em2.o$b[lt,1]));
+        }
+        pdf(paste("Type2fit-",sampleID,".pdf",sep=""),width=6,height=4);
+        plot(density(beta2.v));
+        d.o = density(tmpB.v);
+        points(d.o$x,d.o$y,col="green",type="l")
+        legend(x=0.5,y=3,legend=c("obs","fit"),fill=c("black","green"),bty="n");
+        dev.off();
+    }
 
-classAV1.v = vector();classAV2.v = vector();
-for(l in 1:nL){
-  classAV1.v[l] =  em1.o$mu[l,1];
-  classAV2.v[l] =  em2.o$mu[l,1];
-}
+    classAV1.v = vector();classAV2.v = vector();
+    for(l in 1:nL){
+        classAV1.v[l] =  em1.o$mu[l,1];
+        classAV2.v[l] =  em2.o$mu[l,1];
+    }
 
-### start normalising input probes
-print("Start normalising input probes");
-nbeta2.v = beta2.v;
-### select U probes
-lt = 1;
-selU.idx = which(class2.v==lt);
-selUR.idx = selU.idx[which(beta2.v[selU.idx] > classAV2.v[lt])];
-selUL.idx = selU.idx[which(beta2.v[selU.idx] < classAV2.v[lt])];
-### find prob according to typeII distribution
-p.v = pbeta(beta2.v[selUR.idx],em2.o$a[lt,1],em2.o$b[lt,1],lower.tail=FALSE);
-### find corresponding quantile in type I distribution
-q.v = qbeta(p.v,em1.o$a[lt,1],em1.o$b[lt,1],lower.tail=FALSE);
-nbeta2.v[selUR.idx] = q.v;
-p.v = pbeta(beta2.v[selUL.idx],em2.o$a[lt,1],em2.o$b[lt,1],lower.tail=TRUE);
-### find corresponding quantile in type I distribution
-q.v = qbeta(p.v,em1.o$a[lt,1],em1.o$b[lt,1],lower.tail=TRUE);
-nbeta2.v[selUL.idx] = q.v;
+    ## start normalising input probes
+    print("Start normalising input probes");
+    nbeta2.v = beta2.v;
+    ## select U probes
+    lt = 1;
+    selU.idx = which(class2.v==lt);
+    selUR.idx = selU.idx[which(beta2.v[selU.idx] > classAV2.v[lt])];
+    selUL.idx = selU.idx[which(beta2.v[selU.idx] < classAV2.v[lt])];
+    ## find prob according to typeII distribution
+    p.v = pbeta(beta2.v[selUR.idx],em2.o$a[lt,1],em2.o$b[lt,1],lower.tail=FALSE);
+    ## find corresponding quantile in type I distribution
+    q.v = qbeta(p.v,em1.o$a[lt,1],em1.o$b[lt,1],lower.tail=FALSE);
+    nbeta2.v[selUR.idx] = q.v;
+    p.v = pbeta(beta2.v[selUL.idx],em2.o$a[lt,1],em2.o$b[lt,1],lower.tail=TRUE);
+    ## find corresponding quantile in type I distribution
+    q.v = qbeta(p.v,em1.o$a[lt,1],em1.o$b[lt,1],lower.tail=TRUE);
+    nbeta2.v[selUL.idx] = q.v;
 
-### select M probes
-lt = 3;
-selM.idx = which(class2.v==lt);
-selMR.idx = selM.idx[which(beta2.v[selM.idx] > classAV2.v[lt])];
-selML.idx = selM.idx[which(beta2.v[selM.idx] < classAV2.v[lt])];
-### find prob according to typeII distribution
-p.v = pbeta(beta2.v[selMR.idx],em2.o$a[lt,1],em2.o$b[lt,1],lower.tail=FALSE);
-### find corresponding quantile in type I distribution
-q.v = qbeta(p.v,em1.o$a[lt,1],em1.o$b[lt,1],lower.tail=FALSE);
-nbeta2.v[selMR.idx] = q.v;
+    ## select M probes
+    lt = 3;
+    selM.idx = which(class2.v==lt);
+    selMR.idx = selM.idx[which(beta2.v[selM.idx] > classAV2.v[lt])];
+    selML.idx = selM.idx[which(beta2.v[selM.idx] < classAV2.v[lt])];
+    ## find prob according to typeII distribution
+    p.v = pbeta(beta2.v[selMR.idx],em2.o$a[lt,1],em2.o$b[lt,1],lower.tail=FALSE);
+    ## find corresponding quantile in type I distribution
+    q.v = qbeta(p.v,em1.o$a[lt,1],em1.o$b[lt,1],lower.tail=FALSE);
+    nbeta2.v[selMR.idx] = q.v;
 
 
-if(doH){ ### if TRUE also correct type2 hemimethylated probes
-### select H probes and include ML probes (left ML tail is not well described by a beta-distribution).
-lt = 2;
-selH.idx = c(which(class2.v==lt),selML.idx);
-minH = min(beta2.v[selH.idx],na.rm=TRUE)
-maxH = max(beta2.v[selH.idx],na.rm=TRUE)
-deltaH = maxH - minH;
-#### need to do some patching
-deltaUH = -max(beta2.v[selU.idx],na.rm=TRUE) + min(beta2.v[selH.idx],na.rm=TRUE)
-deltaHM = -max(beta2.v[selH.idx],na.rm=TRUE) + min(beta2.v[selMR.idx],na.rm=TRUE)
+    if(doH){ ### if TRUE also correct type2 hemimethylated probes
+        ## select H probes and include ML probes (left ML tail is not well described by a beta-distribution).
+        lt = 2;
+        selH.idx = c(which(class2.v==lt),selML.idx);
+        minH = min(beta2.v[selH.idx],na.rm=TRUE)
+        maxH = max(beta2.v[selH.idx],na.rm=TRUE)
+        deltaH = maxH - minH;
+        ## need to do some patching
+        deltaUH = -max(beta2.v[selU.idx],na.rm=TRUE) + min(beta2.v[selH.idx],na.rm=TRUE)
+        deltaHM = -max(beta2.v[selH.idx],na.rm=TRUE) + min(beta2.v[selMR.idx],na.rm=TRUE)
 
-## new maximum of H probes should be
-nmaxH = min(nbeta2.v[selMR.idx],na.rm=TRUE) - deltaHM;
-## new minimum of H probes should be
-nminH = max(nbeta2.v[selU.idx],na.rm=TRUE) + deltaUH;
-ndeltaH = nmaxH - nminH;
+        ## new maximum of H probes should be
+        nmaxH = min(nbeta2.v[selMR.idx],na.rm=TRUE) - deltaHM;
+        ## new minimum of H probes should be
+        nminH = max(nbeta2.v[selU.idx],na.rm=TRUE) + deltaUH;
+        ndeltaH = nmaxH - nminH;
 
-### perform conformal transformation (shift+dilation)
-## new_beta_H(i) = a + hf*(beta_H(i)-minH);
-hf = ndeltaH/deltaH ;
-### fix lower point first
-nbeta2.v[selH.idx] = nminH + hf*(beta2.v[selH.idx]-minH);
+        ## perform conformal transformation (shift+dilation)
+        ## new_beta_H(i) = a + hf*(beta_H(i)-minH);
+        hf = ndeltaH/deltaH ;
+        ## fix lower point first
+        nbeta2.v[selH.idx] = nminH + hf*(beta2.v[selH.idx]-minH);
+    }
 
-}
+    pnbeta.v = beta.v;
+    pnbeta.v[type1.idx] = beta1.v;
+    pnbeta.v[type2.idx] = nbeta2.v;
 
-pnbeta.v = beta.v;
-pnbeta.v[type1.idx] = beta1.v;
-pnbeta.v[type2.idx] = nbeta2.v;
+    ## generate final plot to check normalisation
+    if(plots){
+        print("Generating final plot");
+        d1.o = density(beta1.v);
+        d2.o = density(beta2.v);
+        d2n.o = density(nbeta2.v);
+        ymax = max(d2.o$y,d1.o$y,d2n.o$y);
+        pdf(paste("CheckBMIQ-",sampleID,".pdf",sep=""),width=6,height=4)
+        plot(density(beta2.v),type="l",ylim=c(0,ymax),xlim=c(0,1));
+        points(d1.o$x,d1.o$y,col="red",type="l");
+        points(d2n.o$x,d2n.o$y,col="blue",type="l");
+        legend(x=0.5,y=ymax,legend=c("type1","type2","type2-BMIQ"),
+               bty="n",fill=c("red","black","blue"));
+        dev.off();
+    }
 
-### generate final plot to check normalisation
-if(plots){
- print("Generating final plot");
- d1.o = density(beta1.v);
- d2.o = density(beta2.v);
- d2n.o = density(nbeta2.v);
- ymax = max(d2.o$y,d1.o$y,d2n.o$y);
- pdf(paste("CheckBMIQ-",sampleID,".pdf",sep=""),width=6,height=4)
- plot(density(beta2.v),type="l",ylim=c(0,ymax),xlim=c(0,1));
- points(d1.o$x,d1.o$y,col="red",type="l");
- points(d2n.o$x,d2n.o$y,col="blue",type="l");
- legend(x=0.5,y=ymax,legend=c("type1","type2","type2-BMIQ"),bty="n",fill=c("red","black","blue"));
- dev.off();
-}
-
-print(paste("Finished for sample ",sampleID,sep=""));
-
-return(list(nbeta=pnbeta.v,class1=class1.v,class2=class2.v,av1=classAV1.v,av2=classAV2.v,hf=hf,th1=nth1.v,th2=th2.v));
-
+    print(paste("Finished for sample ",sampleID,sep=""));
+    
+    return(list(nbeta=pnbeta.v,class1=class1.v,class2=class2.v,av1=classAV1.v,av2=classAV2.v,hf=hf,th1=nth1.v,th2=th2.v));
 }
 
 
