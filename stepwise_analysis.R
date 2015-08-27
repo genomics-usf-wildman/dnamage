39906aed7f72e02cb1fa22c378a731987a0ec3b0
Don Armstrong <don@donarmstrong.com>
2015-08-27 11:46:54 -0700

    add datClock and anti.trafo functions to stepwise analysis

--- stepwise_analysis.R
+++ stepwise_analysis.R
@@ -3,26 +3,47 @@
 # and whose first column reports the CpG identifier
 # and whose remaining columns corresponds to samples (e.g. Illumina arrays).
 
-estimate.dnam.age <- function(data,goldstandard="probeAnnotation21kdatMethUsed.csv",fastImputation=FALSE) {
+##' Estimation of DNAm Age
+##'
+##' .. content for \details{} ..
+##' @title 
+##' @param data -- data frame whose rows correspond to CpGs and first
+##' column is the CpG identifier
+##' @param goldstandard -- name of the gold standard probe 21k data file
+##' @param dat.clock -- AdditionalFile3.csv from horvath
+##' @param trafo -- age transformation function.
+##' @param anti.trafo -- inverse age transformation function
+##' @param fastImputation 
+##' @param meanXChromosome 
+##' @return 
+##' @author Steve Horvath
+estimate.dnam.age <- function(data,goldstandard="probeAnnotation21kdatMethUsed.csv",dat.clock="AdditionalFile3.csv",trafo=function(x,adult.age=20){x <- (x+1)/(1+adult.age); ifelse(x<=1,log(x),x-1)},anti.trafo=function(x,adult.age=20){ifelse(x<0,(1+adult.age)*exp(x)-1,(1+adult.age)*x+adult.age)},fastImputation=FALSE,meanXchromosome=NULL) {
     ## if goldstandard is a character or a connection it must be a
     ## filename; read it.
-    if (inherits(goldstandard,"connection")||
-        is.character(goldstandard)=="character") {
+    if (inherits(goldstandard,"connection") ||
+        is.character(goldstandard)) {
         goldstandard <- read.csv(goldstandard)
     }
+    if (inherits(dat.clock,"connection") ||
+        is.character(dat.clock)) {
+        dat.clock <- read.csv(dat.clock)
+    }
+    
     
     ##STEP 1: DEFINE QUALITY METRICS
 
     meanMethBySample <- as.numeric(apply(as.matrix(data[,-1]),2,mean,na.rm=TRUE))
     minMethBySample <- as.numeric(apply(as.matrix(data[,-1]),2,min,na.rm=TRUE))
     maxMethBySample <- as.numeric(apply(as.matrix(data[,-1]),2,max,na.rm=TRUE))
-
+    
     datMethUsed <- t(data[,-1])
     colnames(datMethUsed) <- as.character(data[,1])
 
     noMissingPerSample <- apply(as.matrix(is.na(datMethUsed)),1,sum)
     table(noMissingPerSample)
 
+    nSamples <- nrow(datMethUsed)
+    
     ##STEP 2: Imputing 
     if (! fastImputation & nSamples>1 &
         max(noMissingPerSample,na.rm=TRUE)<3000 ){
@@ -74,36 +95,36 @@ estimate.dnam.age <- function(data,goldstandard="probeAnnotation21kdatMethUsed.c
     }
     rm(datMethUsed); gc()
 
-
+    print(dat.clock[1:5,])
 
 
 
     ## STEP 4: Predict age and create a data frame for the output (referred to as datout)
-    selectCpGsClock=is.element(dimnames(datMethUsedNormalized)[[2]],
-        as.character(datClock$CpGmarker[-1]))
-    if ( sum( selectCpGsClock) < dim(datClock)[[1]]-1 ) {
+    selectCpGsClock=is.elemenqt(dimnames(datMethUsedNormalized)[[2]],
+        as.character(dat.clock$CpGmarker[-1]))
+    if ( sum( selectCpGsClock) < dim(dat.clock)[[1]]-1 ) {
         stop(paste0("The CpGs listed in column 1 of the input data did ",
                     "not contain the CpGs needed for calculating DNAm age. ",
                     "Make sure to input cg numbers such as cg00075967."))
     }
-    if ( sum( selectCpGsClock) > dim(datClock)[[1]]-1 ) {
+    if ( sum( selectCpGsClock) > dim(dat.clock)[[1]]-1 ) {
         stop(paste0("ERROR: The CpGs listed in column 1 of the input data ",
                     "contain duplicate CpGs. Each row should report only ",
                     "one unique CpG marker (cg number)."))}
     if (nSamples>1 ) {
         datMethClock0=data.frame(datMethUsedNormalized[,selectCpGsClock])
-        datMethClock= data.frame(datMethClock0[ as.character(datClock$CpGmarker[-1])])
+        datMethClock= data.frame(datMethClock0[ as.character(dat.clock$CpGmarker[-1])])
         dim(datMethClock)
-        predictedAge=as.numeric(anti.trafo(datClock$CoefficientTraining[1]+as.matrix(datMethClock)%*% as.numeric(datClock$CoefficientTraining[-1])))
+        predictedAge=as.numeric(anti.trafo(dat.clock$CoefficientTraining[1]+as.matrix(datMethClock)%*% as.numeric(dat.clock$CoefficientTraining[-1])))
     } # end of if
 
 
     if (nSamples==1 ) {
         datMethUsedNormalized2=data.frame(rbind(datMethUsedNormalized,datMethUsedNormalized))
         datMethClock0=data.frame(datMethUsedNormalized2[,selectCpGsClock])
-        datMethClock= data.frame(datMethClock0[ as.character(datClock$CpGmarker[-1])])
+        datMethClock= data.frame(datMethClock0[ as.character(dat.clock$CpGmarker[-1])])
         dim(datMethClock)
-        predictedAge=as.numeric(anti.trafo(datClock$CoefficientTraining[1]+as.matrix(datMethClock)%*% as.numeric(datClock$CoefficientTraining[-1])))
+        predictedAge=as.numeric(anti.trafo(dat.clock$CoefficientTraining[1]+as.matrix(datMethClock)%*% as.numeric(dat.clock$CoefficientTraining[-1])))
         predictedAge=predictedAge[1]
     } # end of if
     
@@ -117,13 +138,13 @@ estimate.dnam.age <- function(data,goldstandard="probeAnnotation21kdatMethUsed.c
     Comment[is.na(predictedAge)]="Age prediction was not possible. "
 
 
-    if ( sum( selectCpGsClock) < dim(datClock)[[1]]-1 ) {
+    if ( sum( selectCpGsClock) < dim(dat.clock)[[1]]-1 ) {
         Comment=rep(paste0("ERROR: The CpGs listed in column 1 of the input data ",
             "did not contain the CpGs needed for calculating DNAm age. ",
             "Make sure to input cg numbers such as cg00075967."),
             length(predictedAge) )
     }
-    if ( sum( selectCpGsClock) > dim(datClock)[[1]]-1 ) {
+    if ( sum( selectCpGsClock) > dim(dat.clock)[[1]]-1 ) {
         Comment=rep(paste0("ERROR: The CpGs listed in column 1 of the input data ",
             "contain duplicate CpGs. Each row should report only one ",
             "unique CpG marker (cg number).",
